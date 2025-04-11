ebd3f776e7b3f78f7600771725214bab02e9ed02
egoitzaulestia <egoitzaulestia@gmail.com>
2025-04-11 22:06:41 +0200

    Gallery almost done

--- scripts.js
+++ scripts.js
@@ -30,19 +30,55 @@ const militaryHelicopter = [
     'assets/helicoptero-militar-5.jpg'
 ];
 
+
+// 1. **Gallery**: Su única funcionalidad es albergar imágenes y devolverlas dependiendo del método que utilicemos. No interactúa con HTML.
+//    - `constructor`: Acepta dos parámetros, ambos conjuntos de imágenes.
+//    - `getRandomCivil`: devuelve un vehículo aleatorio civil de la galería.
+//    - `getRandomMilitary`: devuelve un vehículo aleatorio militar de la galería.
+//    - `getAll`: devuelve el conjunto de vehículos de la galería, tanto militares como civiles.
+
 class Gallery {
     constructor(civilImages, militaryImages) {
+        this.civilImages = civilImages;
+        this.militaryImages = militaryImages;
     }
     
     getRandomCivil() {
+        const image = this.civilImages[Math.floor((Math.random() * this.civilImages.length))];
+        console.log(image);
+        return image;
+        // return this.civilImages[Math.floor((Math.random() * this.civilImages.length))]
     }
     
     getRandomMilitary() {
+        const militaryImage = this.militaryImages[Math.floor((Math.random() * this.civilImages.length))];
+        console.log(militaryImage);
+        return militaryImage;
+        // return this.militaryImages[Math.floor((Math.random() * this.militaryImages.length))];
     }
     
     getAll() {
     }
 }
+
+const moma = new Gallery(civilAircrafts, militaryAircrafts)
+
+moma.getRandomCivil()
+moma.getRandomMilitary()
+
+// 2. **Painter**: La clase encargada de pintar las imágenes, de interactuar con el DOM. Encargada de crear etiquetas y manipular el DOM para agregarlas.
+//     - `constructor`: Ejecutará la función `createGallery`.
+//     - `createGallery`:
+//         - Creará un elemento `section` y lo agregará al body.
+//         - Dicho `section` será, también, una propiedad de Painter a la que llamaremos `gallery`.
+//     - `createImageTag`: Acepta la url de una imagen y devuelve los siguientes elementos:
+//         ```javascript
+//         <picture>
+//             <img src="ThisIsAnImage.jpg" />
+//         </picture>
+//         ```
+//     - `paintSingleImage`: Acepta la url de una imagen y agrega a `gallery` el elemento creado por `createImageTag`.
+//     - `paintMultipleImages`: Acepta un conjunto de imágenes y agrega a `gallery`, uno a uno, el elemento creado por `createImageTag`.
 
 class Painter {
     constructor() {
